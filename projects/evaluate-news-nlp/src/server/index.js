eede2317447e1f187414d60ac7c30e0521abfd0d
ReemElayman <47919284+ReemElayman@users.noreply.github.com>
2021-03-19 16:09:54 +0200

    stage 2-  Setting up meaningCloud API

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,17 +1,51 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
+var request = require('request');
+var bodyParser = require('body-parser')
+const axios = require('axios');
+
 const mockAPIResponse = require('./mockAPI.js')
 
-const app = express()
 
+const app = express()
+app.use(bodyParser.json())
 app.use(express.static('dist'))
 
-console.log(__dirname)
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
+
+
+
+//evaluating URL post request
+app.post('/evaluate',async  function (req, res) {
+    console.log(req.body)
+    const {url}= req.body
+
+    var meaningcloudUrl = `https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&url=${url}&lang=en`
+    var analysis;
+    
+   await axios.get(meaningcloudUrl)
+      .then(async function (response) {
+        analysis= {"agreement":response.data.agreement,
+        "score_tag":response.data.score_tag,
+        "subjectivity":response.data.subjectivity,
+        "confidence":response.data.confidence,
+        "irony":response.data.irony}
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+
+     res.send(analysis);
+});
+
+
 
 // designates what port the app will listen to for incoming requests
 app.listen(8082, function () {
