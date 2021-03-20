7afe5d42ec5694e37997645d6a660d84f175e90e
ReemElayman <47919284+ReemElayman@users.noreply.github.com>
2021-03-20 17:54:38 +0200

    Stage 5 Service Workers Added

--- projects/evaluate-news-nlp/webpack.prod.js
+++ projects/evaluate-news-nlp/webpack.prod.js
@@ -1,10 +1,17 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+        },
     module: {
         rules: [
             {
@@ -14,8 +21,8 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-            },
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+                },
               {
                 test: /\.(png|jpe?g|gif)$/i,
                 loader: 'file-loader',
@@ -33,6 +40,8 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
